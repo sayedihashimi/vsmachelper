f28b09d723c01161bcc13d3e31e40607fd54c6c8
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2019-07-03 23:48:47 -0400

    git init

---
+++ VsmacHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VsmacHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
